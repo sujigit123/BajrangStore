caca9b2809802b1e499bb932e784bdf85f287c85
Sujit Kumar Sahoo <sujit1.dev@gmail.com>
2026-05-09 23:38:41 +0530

    Modified some apis

---
+++ MegaStoreProj/Services/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok("Test Product");
+        }
+    }
+}
